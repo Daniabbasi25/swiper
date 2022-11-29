f71bf303f5ad19db5cf08c2ef5132d1febd7ea27
Danyal Muneer <abbasidani82@gmail.com>
2022-11-29 15:06:15 +0500

    first commit

---
+++ app/services/socialLoginConfig.js
@@ -0,0 +1,14 @@
+export const socialConfig = {
+  enabled: true,
+  socialPlatforms: ["facebook", "google"], // Currently we only support Facebook and Google signin. We'll add more in future
+  google: {
+    webClientId:
+      "422661746750-cvicau6js9btjtigic16fh104ca6bf4v.apps.googleusercontent.com",
+  },
+  facebook: {
+    appID: "319269513458188",
+    appName: "MauDeal",
+    android: {},
+    iOS: {},
+  },
+};
---
+++ variables/color.js
@@ -0,0 +1,51 @@
+export const COLORS = {
+  primary: "#DD0000",
+  primary_soft: "#DD3333",
+  bg_primary: "#DD3333",
+
+  button: {
+    disabled: "#f797a3",
+    active: "#DD0000",
+  },
+
+  bg_splash: "#DD0000",
+
+  badges: {
+    _top: "#169DB3",
+    featured: "#E0AA06",
+    _bump_up: "#f39f0a",
+    "is-sold": "#1aa78e",
+    "is-popular": "#14e0ad",
+  },
+
+  cardBg: {
+    _top: "#FFFFFF",
+    featured: "#e5f8f7",
+    "is-popular": "#c4fff0",
+  },
+
+  bg_light: "#f9f9f9",
+  bg_dark: "#f0f0f0",
+  text_gray: "#838383",
+  text_dark: "#111",
+  white: "#fff",
+
+  black: "#000",
+  gray: "gray",
+  blue: "#00f",
+  red: "#f00",
+  dark_green: "#155724",
+
+  green: "#25A01B",
+  light_green: "#d4edda",
+  yellow: "yellow",
+  yellow_dark: "#e0cb2d",
+
+  dodgerblue: "dodgerblue",
+  orange: "#ff6600",
+  border_light: "#cfcfcf",  
+  border_dark: "gray",
+  packageCardHeader: "#8a6fdf",
+  rating_star: "#ffb400",
+
+};
